93ceb9100ca4f3b19fdbc1072fd904f6e251544d
antonioqneto <antoniodevneto@gmail.com>
2022-08-08 23:40:31 -0300

    First commit

---
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -0,0 +1,34 @@
+package me.dio.soccernews.ui.news;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dio.soccernews.domain.News;
+
+public class NewsViewModel extends ViewModel {
+
+    private final MutableLiveData<List<News>> news;
+
+    public NewsViewModel() {
+        this.news= new MutableLiveData<>();
+
+        //TODO Remover Mock de notícias
+        List<News> news = new ArrayList<>();
+        news.add(new News("Ararinha completaria 247 anos neste fim de semana", "\n" +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam diam orci, aliquam."));
+        news.add(new News("Roberto Carlos anuncia aquisição de USJ", "\n" +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam diam orci, aliquam."));
+        news.add(new News("Estão abertas as inscrições para escolinha do USJ", "\n" +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam diam orci, aliquam."));
+
+        this.news.setValue(news);
+    }
+
+    public LiveData<List<News>> getNews() {
+        return news;
+    }
+}
